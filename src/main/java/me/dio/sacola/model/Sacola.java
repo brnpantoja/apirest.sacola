aee93c6fbf39f1b2e34152a2623ef5450716668d
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-09-29 18:25:00 -0300

    Implementando métodos

--- src/main/java/me/dio/sacola/model/Sacola.java
+++ src/main/java/me/dio/sacola/model/Sacola.java
@@ -1,9 +1,22 @@
 package me.dio.sacola.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import me.dio.sacola.enumeration.FormaPagamento;
 
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+import java.util.List;
 
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@NoArgsConstructor
 public class Sacola {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -11,11 +24,12 @@ public class Sacola {
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JsonIgnore
     private Cliente cliente;
-    @OneToMan(cascade = CascadeType.All)
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Item> itens;
     private Double valorTotal;
     @Enumerated
     private FormaPagamento formaPagamento;
     private boolean fechada;
+
 
 }
