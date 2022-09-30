35c04d750f5dc59c8ffa5abed9e92d5a8357662d
brnpantoja <treinadorbrunopantoja@gmail.com>
2022-09-30 18:03:40 -0300

    Finalizando

---
+++ readme.md
@@ -0,0 +1,6 @@
+Projeto de Aplicação de Delivery
+
+Java 17
+Spring Boot
+Swagger
+H2-console
--- src/main/java/me/dio/sacola/resource/SacolaResource.java
+++ src/main/java/me/dio/sacola/resource/SacolaResource.java
@@ -1,12 +1,13 @@
 package me.dio.sacola.resource;
 
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import me.dio.sacola.model.Item;
 import me.dio.sacola.model.Sacola;
 import me.dio.sacola.resource.dto.ItemDto;
 import me.dio.sacola.service.SacolaService;
 import org.springframework.web.bind.annotation.*;
-
+@Api(value="/ifood-devweek/sacolas")
 @RestController
 @RequestMapping("/ifood-devweek/sacolas")
 @RequiredArgsConstructor
